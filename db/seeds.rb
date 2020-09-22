fc0fcd7143e5b06e9918086e388dbcc563346697
fjswayze <fjswayze@vassar.edu>
2020-09-22 14:49:37 -0700

    heroku fix pt2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+testuser = User.create([{email: 'test1997@gmail.com'}, {password: 'hello1997'}])
