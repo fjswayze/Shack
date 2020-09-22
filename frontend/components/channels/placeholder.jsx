3adcbfe97c7bbdbcb5415514dd6528ab35873edd
fjswayze <fjswayze@vassar.edu>
2020-09-22 14:13:52 -0700

    shackURL?

--- frontend/components/channels/placeholder.jsx
+++ frontend/components/channels/placeholder.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Placeholder = (props) => (
     <div>
         <h1>Placeholder</h1>
-        <img src={window.shackURL} />        
+        {/* <img src={window.shackURL} />         */}
         <button onClick={() => props.action()}>Log Out</button>
 
     </div>
