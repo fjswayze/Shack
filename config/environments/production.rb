880c761d122dc2c9d75c8d422873f933caf3edfe
fjswayze <fjswayze@vassar.edu>
2020-10-29 11:12:15 -0700

    fixed syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://shack-aa.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://shack-aa.herokuapp.com', /https:\/\/shack-aa.herokuapp.com/*]
+  config.action_cable.allowed_request_origins = [ 'https://shack-aa.herokuapp.com', /https:\/\/shack-aa.herokuapp.com*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
