f1838514725595585e4a1aafb5432256e61bf208
fjswayze <fjswayze@vassar.edu>
2020-11-03 08:24:42 -0800

    commented out hack destroy

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
       get 'channel_memberships/:channel_id/:user_id/', to: 'channel_memberships#hack_destroy'
 
-      delete 'dm_memberships/:dm_id/:user_id/', to: 'dm_memberships#hack_destroy'
+      # delete 'dm_memberships/:dm_id/:user_id/', to: 'dm_memberships#hack_destroy'
 
 
 
